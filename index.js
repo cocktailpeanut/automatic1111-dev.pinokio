f82d8cb475050689ac2558a0c430529e4cd00903
cocktailpeanut <cocktailpeanuts@proton.me>
2023-07-14 16:03:26 -0400

    --no-half-vae

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ class Automatic1111 {
       let newtext = text.replace(re, `$2"${defaultArgs}"`)
       await fs.promises.writeFile(path.resolve(__dirname, "automatic1111", "webui-user.sh"), newtext)
     } else if (this.platform === 'win32') {
-      let defaultArgs = "--api"
+      let defaultArgs = "--api --no-half-vae"
       let text = await fs.promises.readFile(path.resolve(__dirname, "automatic1111", "webui-user.bat"), "utf8")
       let re = /^(set COMMANDLINE_ARGS=)(.*)$/m
       let newtext = text.replace(re, `$1"${defaultArgs}"`)
@@ -51,9 +51,9 @@ class Automatic1111 {
       let defaultArgs
       if (/amd/i.test(this.vendor)) {
         // lshqqytiger
-        defaultArgs = "--precision full --no-half --api"
+        defaultArgs = "--precision full --no-half --api --no-half-vae"
       } else {
-        defaultArgs = "--api"
+        defaultArgs = "--api --no-half-vae"
       }
       let text = await fs.promises.readFile(path.resolve(__dirname, "automatic1111", "webui-user.sh"), "utf8")
       let re = /^(#?)(export COMMANDLINE_ARGS=)(.+)$/m
